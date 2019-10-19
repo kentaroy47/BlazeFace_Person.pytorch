da13fafd65ee07f5375659b312abb38f7752259c
kentaroy47 <kentaroy@stanford.edu>
2019-10-19 02:51:46 +0000

    add lr scheduler

--- utils/blazeface.py
+++ utils/blazeface.py
@@ -165,17 +165,28 @@ def make_loc_conf(num_classes=2, bbox_aspect_num=[6, 6]):
     loc_layers = []
     conf_layers = []
     
-    # VGGの中間出力に対するレイヤ
-    loc_layers += [nn.Conv2d(96, bbox_aspect_num[0] * 4,
-                            kernel_size=3, padding=1)]
-    conf_layers += [nn.Conv2d(96, bbox_aspect_num[0] * num_classes,
-                             kernel_size=3, padding=1)]
+    # added more layers.
+    loc_layers += [nn.Sequential(
+                nn.Conv2d(96, 192, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, bbox_aspect_num[0] * 4, kernel_size=3, padding=1))
+                ]
+    conf_layers += [nn.Sequential(
+                nn.Conv2d(96, 192, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, bbox_aspect_num[0] * num_classes, kernel_size=3, padding=1))]
     
-    # VGGの最終そうに対するCNN
-    loc_layers += [nn.Conv2d(96, bbox_aspect_num[1] * 4,
-                            kernel_size=3, padding=1)]
-    conf_layers += [nn.Conv2d(96, bbox_aspect_num[1] * num_classes,
-                             kernel_size=3, padding=1)]
+    # 
+    loc_layers += [nn.Sequential(
+                nn.Conv2d(96, 192, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, bbox_aspect_num[1] * 4, kernel_size=3, padding=1))
+                ]
+    conf_layers += [nn.Sequential(
+                nn.Conv2d(96, 192, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, bbox_aspect_num[1] * num_classes, kernel_size=3, padding=1))
+                ]
     return nn.ModuleList(loc_layers), nn.ModuleList(conf_layers)
 
 loc_test, conf_test = make_loc_conf()
